0cd9863c57aa4eca0d971eb5e6e77afd0c55f3b4
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-09-28 16:38:22 -0500

    Update prepare-sql.sh

--- prepare-sql.sh
+++ prepare-sql.sh
@@ -9,6 +9,8 @@ apt install -y default-jdk
 LOCAL_DIR=`pwd`
 CERT_DIR="${LOCAL_DIR}/certs"
 
+cp /etc/ssl/certs/java/cacerts ${CERT_DIR}
+
 openssl s_client -showcerts -connect ${CL_HOST}:$PORT < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${CERT_DIR}/ca.crt
 
 keytool -import -trustcacerts -keystore ${CERT_DIR}/cacerts -storepass changeit -noprompt -alias codelogic -file ${CERT_DIR}/ca.crt
