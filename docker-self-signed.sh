d3adb495c08151b7fa2eb93177ad81cfc5804989
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-09-28 10:45:06 -0500

    Create docker-self-signed.sh

---
+++ docker-self-signed.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# CodeLogic host for registry access.
+HOST="<YOUR_CODELOGIC_HOST>"
+# CodeLogic port for registry access.
+PORT="443"
+
+# Remove original daemon.json
+rm -f /etc/docker/daemon.json
+
+# Create empty daemon.json
+cat << EOF > /etc/docker/daemon.json
+{ }
+EOF
+
+# Make directory reflecting host exposing self-signed cert.
+mkdir -p /etc/docker/certs.d/${HOST}
+
+# Retrieve certificate from host and place in docker cert location.
+openssl s_client -showcerts -connect ${HOST}:$PORT < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /etc/docker/certs.d/${HOST}/ca.crt
+
+# Stop CodeLogic.
+systemctl stop codelogic
+
+sleep 10
+
+# Restart docker.
+systemctl restart docker
+
+sleep 10
+
+# For whatever reason the docker daemon will start a subset of CodeLogic containers. Stop orphans.
+docker stop $(docker ps -q)
+
+sleep 10
+
+# Start CodeLogic
+systemctl start codelogic
