b8af9d85c776b515becc5da99382114811c91181
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-09-28 16:03:29 -0500

    Create prepare-sql.sh

---
+++ prepare-sql.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Requires java.
+apt install -y default-jdk
+
+# Source in agent environment variables.
+[ ! -f .agent_env ] || export $(grep -v '^#' .agent_env | xargs)
+
+LOCAL_DIR=`pwd`
+CERT_DIR="${LOCAL_DIR}/certs"
+
+openssl s_client -showcerts -connect ${CL_HOST}:$PORT < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${CERT_DIR}/ca.crt
+
+keytool -import -trustcacerts -keystore ${CERT_DIR}/cacerts -storepass changeit -noprompt -alias codelogic -file ${CERT_DIR}/ca.crt
