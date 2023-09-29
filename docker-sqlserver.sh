87296f2d40831aa2830a4b801780cd5796d78e8b
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-09-29 09:18:51 -0500

    Update and rename docker-sql.sh to docker-sqlserver.sh

--- docker-sqlserver.sh
+++ docker-sqlserver.sh
@@ -6,9 +6,9 @@
 LOCAL_DIR=`pwd`
 CERT_DIR="${LOCAL_DIR}/certs"
 
-# analyze -a DNN -c jdbc:sqlserver://192.168.0.63;databaseName=dnn;trustServerCertificate=false;encrypt=false -u SA -pwd Stup!dE@sy --expunge-scan-sessions
+# analyze -a DNN -c jdbc:sqlserver://192.168.0.63;databaseName=dnn;trustServerCertificate=false;encrypt=false -u theuser -pwd thepass --expunge-scan-sessions
 # Must modify $CL_SQLCMD to reflect the proper connection strinng as shown above.  Need databaseName=
-CL_SQLCMD="analyze -a ${DB_APPNAME} -c jdbc:${DB_TYPE}://${DB_HOST}:${DB_PORT}/${DB_NAME};${DB_XTRA} -u ${DB_USER} -pwd ${DB_PASS} --expunge-scan-sessions"
+CL_SQLCMD="analyze -a ${DB_APPNAME} -c jdbc:${DB_TYPE}://${DB_HOST};databaseName=${DB_NAME};${DB_XTRA} -u ${DB_USER} -pwd ${DB_PASS} --expunge-scan-sessions"
 echo ${CL_SQLCMD}
 
 # Run scan with the configured values in .agent_env file.
@@ -18,4 +18,3 @@ docker run --pull always --rm --network host --interactive --tty \
      --env AGENT_UUID=${DB_AGENT_UUID} \
      --env AGENT_PASSWORD=${DB_AGENT_PASSWORD} \
      ${CL_HOST}/codelogic_sql:latest #\
-     #"analyze -a ${DB_APPNAME} -c jdbc:${DB_TYPE}://${DB_HOST}:${DB_PORT}/${DB_NAME}";"${DB_XTRA} -u ${DB_USER} -pwd ${DB_PASS} --expunge-scan-sessions"
