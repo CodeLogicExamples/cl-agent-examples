b404073cc9edb8849d22696a044955e93b11b3c5
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-10-05 16:16:46 -0500

    Create docker-dotnet.sh

---
+++ docker-dotnet.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Source in agent environment variables.
+[ ! -f .dotnetagent_env ] || export $(grep -v '^#' .dotnetagent_env | xargs)
+
+LOCAL_DIR=`pwd`
+CERT_DIR="${LOCAL_DIR}/certs"
+
+# analyze -a DNN -c jdbc:sqlserver://192.168.0.63;databaseName=dnn;trustServerCertificate=false;encrypt=false -u theuser -pwd thepass --expunge-scan-sessions
+# Must modify $CL_SQLCMD to reflect the proper connection strinng as shown above.  Need databaseName=
+#CL_SQLCMD="analyze -a ${DB_APPNAME} -c jdbc:${DB_TYPE}://${DB_HOST};databaseName=${DB_NAME};${DB_XTRA} -u ${DB_USER} -pwd ${DB_PASS} --expunge-scan-sessions"
+#echo ${CL_SQLCMD}
+
+CL_DOTNETCMD=""
+
+# Run scan with the configured values in .agent_env file.
+docker run --pull always --rm --network host --interactive --tty \
+     --volume "${CERT_DIR}/cacerts:/usr/local/openjdk-11/lib/security/cacerts:ro" \
+     --env CODELOGIC_HOST="${PROTOCOL}://${CL_HOST}:${PORT}" \
+     --env AGENT_UUID=${DB_AGENT_UUID} \
+     --env AGENT_PASSWORD=${DB_AGENT_PASSWORD} \
+     ${CL_HOST}/codelogic_dotnet:latest #\
