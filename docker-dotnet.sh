2befd950b01b16e55b09155cd9d1ab4a56ca0043
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-10-12 09:56:40 -0500

    Update docker-dotnet.sh

--- docker-dotnet.sh
+++ docker-dotnet.sh
@@ -3,17 +3,21 @@
 # Source in agent environment variables.
 [ ! -f .dotnetagent_env ] || export $(grep -v '^#' .dotnetagent_env | xargs)
 
-LOCAL_DIR=`pwd`
-CERT_DIR="${LOCAL_DIR}/certs"
-CL_DOTNETCMD=""
+#set -x                                                 # Set for debugging. 
+#DOTNET_CMD="scan-namespaces --help"                    # Help for scan-namespaces.  Used for understanding namespaces to scan
+#DOTNET_CMD="analyze --help"                            # Help for analyze command.
+#DOTNET_CMD="scan-namespaces --path /scan/DotNetNuke"   # Get namespaces from dll files in path.
+# Example dotnet scan command.
+#DOTNET_CMD="analyze -a dnn --path /scan --filter DotNetNuke. --filter DNN. --filter Dnn. --method-filter DotNetNuke. --method-filter DNN. --method-filter Dnn. --database jdbc:sqlserver://192.168.0.63;databaseName=dnn --expunge-scan-sessions"
+DOTNET_CMD="analyze -a ${DOTNET_APPNAME} --path /scan --filter ${DOTNET_DLL_FILTER} --database ${JDBC_STRING} --method-filter ${DOTNET_METHOD_FILTER} --expunge-scan-sessions"
 
 # Run scan with the configured values in .agent_env file.
 docker run --pull always --rm --interactive --tty \
-     --env CODELOGIC_HOST="${PROTOCOL}://${CL_HOST}:${PORT}" \
-     --env AGENT_UUID="<THE_AGENT_UUID>" \
-     --env AGENT_PASSWORD="<THE_AGENT_PASSWORD>" \
+     --env CODELOGIC_HOST="${CL_PROTOCOL}://${CL_HOST}:${CL_PORT}" \
+     --env AGENT_UUID="${DOTNET_AGENT_UUID}" \
+     --env AGENT_PASSWORD="${DOTNET_AGENT_PASSWORD}" \
      --env SSL_CERT_FILE="/mycerts/ca.crt" \
-     --volume "/opt/dnn:/scan" \
-     --volume "/opt/dnn/cl-agent-examples/certs:/mycerts" \
+     --volume "${DOTNET_BINARY_PATH}:/scan" \
+     --volume "${CL_CERT_PATH}:/mycerts" \
      ${CL_HOST}/codelogic_dotnet:latest \
-     ${CL_DOTNETCMD}
+     ${DOTNET_CMD}
